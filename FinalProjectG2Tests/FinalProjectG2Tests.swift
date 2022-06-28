002a647edaec7001e905283e4a3d9a9682fb342d
Isabella <isabellaarevaloann2@gmail.com>
2022-06-28 21:38:19 +0200

    Initial Commit

---
+++ FinalProjectG2Tests/FinalProjectG2Tests.swift
@@ -0,0 +1,36 @@
+//
+//  FinalProjectG2Tests.swift
+//  FinalProjectG2Tests
+//
+//  Created by Isabella Arevalo on 28/6/22.
+//
+
+import XCTest
+@testable import FinalProjectG2
+
+class FinalProjectG2Tests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+        // Any test you write for XCTest can be annotated as throws and async.
+        // Mark your test throws to produce an unexpected failure when your test encounters an uncaught error.
+        // Mark your test async to allow awaiting for asynchronous code to complete. Check the results with assertions afterwards.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
